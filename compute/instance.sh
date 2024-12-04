c462520597c6db0096625477549851aca0323fdb
Maykon <maykon.rds.it@gmail.com>
2024-12-04 09:14:07 -0500

    Update instance.sh

--- compute/instance.sh
+++ compute/instance.sh
@@ -2,5 +2,3 @@
 profile="DEFAULT"
 # List all instances
 oci compute instance list --compartment-id $compartment_id --region us-phoenix-1 --query "data[*].{id:id,\"display-name\":\"display-name\"}" --profile $profile --output table
-
-
